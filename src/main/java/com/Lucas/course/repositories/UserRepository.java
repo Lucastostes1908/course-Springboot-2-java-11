3263805ec2593cd74c820f22dfc57debc8bb0457
Lucas Tostes <lucastostes18@gmail.com>
2020-01-05 19:43:39 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/Lucas/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.Lucas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Lucas.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
