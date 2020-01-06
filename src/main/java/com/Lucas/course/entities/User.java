a607bfe79036bebbc1aaabb2a49ade47c58e8267
Lucas Tostes <lucastostes18@gmail.com>
2020-01-05 23:42:05 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/Lucas/course/entities/User.java
+++ src/main/java/com/Lucas/course/entities/User.java
@@ -1,25 +1,35 @@
 package com.Lucas.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -72,6 +82,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
---
+++ src/main/java/com/Lucas/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.Lucas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Lucas.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
