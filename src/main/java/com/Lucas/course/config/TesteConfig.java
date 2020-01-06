1cf0db9df59c43f0b6d2fe6a78ff394d714013a0
Lucas Tostes <lucastostes18@gmail.com>
2020-01-06 01:42:51 -0300

    Payment, one-to-one association

--- src/main/java/com/Lucas/course/config/TesteConfig.java
+++ src/main/java/com/Lucas/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.Lucas.course.entities.Category;
 import com.Lucas.course.entities.Order;
 import com.Lucas.course.entities.OrderItem;
+import com.Lucas.course.entities.Payment;
 import com.Lucas.course.entities.Product;
 import com.Lucas.course.entities.User;
 import com.Lucas.course.entities.enums.OrderStatus;
@@ -35,7 +36,7 @@ public class TesteConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 
@@ -80,6 +81,10 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 }
